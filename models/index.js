e64d48af6bb8151abfa7c9f5879681b889550f58
Nicole Tribo <Nicoletribo@yahoo.com>
2021-05-03 15:25:35 -0500

    methods with their associations, looks good

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+    foreignKey: 'creator_id',
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'creator_id',
+});
+
+module.exports = { User, Post };
